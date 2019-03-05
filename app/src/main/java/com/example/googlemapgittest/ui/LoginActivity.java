c37768011ebd0a217f434a1b35595cd47022eea0
elven404 <elven404@gmail.com>
2019-03-05 20:54:01 +0900

    a

--- app/src/main/java/com/example/googlemapgittest/ui/LoginActivity.java
+++ app/src/main/java/com/example/googlemapgittest/ui/LoginActivity.java
@@ -1,20 +1,32 @@
 package com.example.googlemapgittest.ui;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.googlemapgittest.R;
+import com.example.googlemapgittest.UserClient;
+import com.example.googlemapgittest.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import static android.text.TextUtils.isEmpty;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -34,51 +46,109 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mProgressBar = findViewById(R.id.progressBar);
 
         setupFirebaseAuth();
+        findViewById(R.id.email_sign_in_button).setOnClickListener(this);
+        findViewById(R.id.link_register).setOnClickListener(this);
+
+        hideSoftKeyboard();
     }
 
     private void setupFirebaseAuth(){
-        Log.d(TAG,"setupFireabaseAuth:started");
-
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if(user != null){
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                    Toast.makeText(LoginActivity.this, "Authenticated with: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, user.getEmail()+"로 로그인 되었습니다.", Toast.LENGTH_SHORT).show();
 
                     FirebaseFirestore db = FirebaseFirestore.getInstance();
-                    FirebaseFirestoreSettings setting = new FirebaseFirestoreSettings.Builder()
-                            .build();
-                    db.setFirestoreSettings(setting);
+                    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder().build();
+                    db.setFirestoreSettings(settings);
 
-                    DocumentReference userRef = db.collection(getString(R.string.collection_users))
-                            .document(user.getUid());
-
+                    DocumentReference userRef = db.collection(getString(R.string.collection_users)).document(user.getUid());
                     userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                             if(task.isSuccessful()){
-                                Log.d(TAG, "onComplete: successfully set the user client.");
                                 User user = task.getResult().toObject(User.class);
                                 ((UserClient)(getApplicationContext())).setUser(user);
                             }
                         }
                     });
-
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                     finish();
                 }else{
-                    Log.d(TAG,"onAuthStateChanged:singed_out");
+                    new AlertDialog.Builder(LoginActivity.this).setMessage("왜 로그아웃 하고 그러느냐!").show();
                 }
             }
         };
     }
 
     @Override
-    public void onClick(View v) {
+    public void onStart() {
+        super.onStart();
+        if(mAuthListener != null){
+            FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+        }
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void signIn(){
+        if(!isEmpty(mEmail.getText().toString())&& !isEmpty(mPassword.getText().toString())){
+            showDialog();
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(mEmail.getText().toString(),
+                    mPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    hideDialog();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                    hideDialog();
+                }
+            });
+        }else{
+            Toast.makeText(this, "모든 칸을 채워야해", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideDialog(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.link_register:{
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.email_sign_in_button:{
+                signIn();
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/googlemapgittest/ui/RegisterActivity.java
+++ app/src/main/java/com/example/googlemapgittest/ui/RegisterActivity.java
@@ -2,13 +2,61 @@ package com.example.googlemapgittest.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
+import com.example.googlemapgittest.R;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class RegisterActivity extends AppCompatActivity {
+import static android.text.TextUtils.isEmpty;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "RegisterActivity";
+
+    private EditText mEmail, mPassword, mConfirmPassword;
+    private ProgressBar mProgressBar;
+
+    //vars
+    private FirebaseFirestore mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mEmail = (EditText) findViewById(R.id.input_email);
+        mPassword = (EditText) findViewById(R.id.input_password);
+        mConfirmPassword = (EditText) findViewById(R.id.input_confirm_password);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        findViewById(R.id.btn_register).setOnClickListener(this);
+
+        mDb = FirebaseFirestore.getInstance();
+
+        hideSoftKeyboard();
+    }
+
+
+    private void hideSoftKeyboard() {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_register: {
+                if (!isEmpty(mEmail.getText().toString())
+                        && !isEmpty(mPassword.getText().toString())
+                        && !isEmpty(mConfirmPassword.getText().toString())) {
+
+                    if()
+
+                }
+            }
+        }
     }
 }
